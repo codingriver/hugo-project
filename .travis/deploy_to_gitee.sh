41928985bb6c69a81923dcb1b0c406ec2863d2f1
codingriver <wraithcorps@163.com>
2020-09-04 20:40:48 +0800

    travis deploy gh-page

---
+++ .travis/deploy_to_gitee.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd public_gitee
+git config user.name "codingriver"
+git config user.email "codingriver@163.com"
+git init
+git add .
+msg="rebuilding site `date`"
+git commit -m "deploy page"
+git remote add origin git@gitee.com:codingriver/codingriver.git
+git push --force origin master:master
+cd ..
